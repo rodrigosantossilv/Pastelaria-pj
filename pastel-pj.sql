30218d827eafb2236bc9c6de168874e99e244509
Rodri.ys <141459048+rodrigosantossilv@users.noreply.github.com>
2023-11-23 20:58:50 -0300

    Update pastel-pj.sql

--- pastel-pj.sql
+++ pastel-pj.sql
@@ -1,93 +1,102 @@
-create database Pastelaria;
-use Pastelaria;
 
-create table clientes(
-id_cliente int not null auto_increment,
-nome_completo varchar(100) not null,
-preferia_por_nome varchar(100) not null,
-cpf int(11)  unique,
-data_nacimento date,
-telefone int(8) unique,
-e_mail varchar(100) unique,
-primary key (id_cliente)
+CREATE DATABASE Pastelaria;
+USE Pastelaria;
+
+CREATE TABLE categorias (
+    id_categoria INT NOT NULL AUTO_INCREMENT,
+    nome_categoria VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_categoria)
 );
 
-create table endereco(
-id_endereco int not null auto_increment,
-id_cliente int not null,
-bairro varchar(50),
-numero VARCHAR(10),
-cep VARCHAR(8),
-municipio VARCHAR(50),
-uf char (2),
-foreign key (id_cliente) references Clientes(id_cliente),
-primary key (id_endereco)
+CREATE TABLE recheios (
+    id_recheios INT NOT NULL AUTO_INCREMENT,
+    nome_recheio VARCHAR(100) NOT NULL,
+    id_categoria INT NOT NULL,
+    PRIMARY KEY (id_recheios),
+    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
 );
 
-create table pedidos(
-id_pedidos int primary key  auto_increment,
-id_cliente int not null,
-id_pasteis int not null,
-quantidade int not null,
-data_pedido date,
-foreign key (id_cliente) references Clientes(id_cliente),
-foreign key(id_pasteis)  references Pasteis(id_pasteis)
+CREATE TABLE pasteis (
+    id_pastel INT NOT NULL AUTO_INCREMENT,
+    nome_pastel VARCHAR(100) NOT NULL,
+    quant_estoque VARCHAR(100) NOT NULL, 
+    tamanho_pastel VARCHAR(20) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    id_recheios INT NOT NULL,
+    PRIMARY KEY(id_pastel),
+    FOREIGN KEY(id_recheios) REFERENCES recheios(id_recheios)
 );
-
-create table categorias(
-id_categoria int not null auto_increment,
-nome_categoria varchar(50) not null,
-primary key (id_categoria)
-);
-
-create table recheios(
-id_recheios int not null auto_increment,
-nome_recheio varchar(100) not null,
-id_pastel varchar(100) not null,
-id_categoria int not null,
-primary key (id_recheios),
-foreign key(id_categoria) references categorias(id_categoria),
-foreign key(id_pastel) references pasteis(id_pastel),    
-);
-
-create table pasteis(
-id_pastel int not null auto_increment,
-nome_pastel varchar(100) not null,
-quant_estoque varchar(100) not null, 
-tamanho_pastel varchar(20) not null,
-preco decimal(10,2) not null,
-id_recheios int not null,
-primary key(id_pasteis),
-foreign key(id_recheios) references Recheios(id_recheios)
-);
-
-
-create table itens_do_pedidos(
-id_itens_do_pedido int  not null primary key auto_increment,
-id_pedidos int not null,
-id_produto int not null ,
-quantidade_itens_pedidos int not null,
-     foreign key(id_pedidos) references Pedidos(id_pedidos)
-    );
-
 
 CREATE TABLE bebidas (
-    id_bebida int not null primary key auto_increment,
-    nome  varchar(100),
-    preco decimal(10,2) not null,
-    quant_estoque int not null default 0,
-    id_categoria int not null,
-    foreign key(id_categoria) references Categorias(id_categoria)
+    id_bebida INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    volume DECIMAL(10,2) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quant_estoque INT NOT NULL DEFAULT 0,
+    id_categoria INT NOT NULL,
+    PRIMARY KEY(id_bebida),
+    FOREIGN KEY(id_categoria) REFERENCES categorias(id_categoria)
 );
 
-CREATE TABLE FormasPagamento (
-    id_forma_pagamento int not null primary key auto_increment,
-    nome_forma_pagamento varchar(100),
-    id_pedidos int,
-    id_itens_do_pedido int,
-    foreign key(id_pedidos) references Pedidos(id_pedidos),
-    foreign key (id_itens_do_pedido) references itens_do_pedidos(id_itens_do_pedido)
+CREATE TABLE clientes (
+    id_cliente INT NOT NULL AUTO_INCREMENT,
+    nome_completo VARCHAR(100) NOT NULL,
+    preferia_por_nome VARCHAR(100) NOT NULL,
+    cpf INT(11) UNIQUE,
+    data_nacimento DATE,
+    telefone INT(8) UNIQUE,
+    e_mail VARCHAR(100) UNIQUE,
+    PRIMARY KEY(id_cliente)
+);
 
+CREATE TABLE endereco (
+    id_endereco INT NOT NULL AUTO_INCREMENT,
+    id_cliente INT NOT NULL,
+    bairro VARCHAR(50),
+    numero VARCHAR(10),
+    cep VARCHAR(8),
+    municipio VARCHAR(50),
+    uf CHAR(2),
+    PRIMARY KEY(id_endereco),
+    FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE pedidos (
+    id_pedidos INT NOT NULL AUTO_INCREMENT,
+    id_cliente INT NOT NULL,
+    id_pastel INT NOT NULL,
+    id_bebidas INT NOT NULL,
+    quantidade INT NOT NULL,
+    data_pedido DATE,
+    PRIMARY KEY(id_pedidos),
+    FOREIGN KEY(id_bebidas) REFERENCES bebidas(id_bebida),
+    FOREIGN KEY(id_pastel) REFERENCES pasteis(id_pastel),
+    FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
+);
+
+CREATE TABLE tamanho (
+    id_tamanho INT NOT NULL AUTO_INCREMENT,
+    id_pastel INT NOT NULL,
+    PRIMARY KEY(id_tamanho),
+    FOREIGN KEY(id_pastel) REFERENCES pasteis(id_pastel)
+);
+
+CREATE TABLE itens_do_pedidos (
+    id_itens_do_pedido INT NOT NULL AUTO_INCREMENT,
+    id_pedidos INT NOT NULL,
+    quantidade_itens_pedidos INT NOT NULL,
+    PRIMARY KEY(id_itens_do_pedido),
+    FOREIGN KEY(id_pedidos) REFERENCES pedidos(id_pedidos)
+);
+
+CREATE TABLE formasPagamento (
+    id_forma_pagamento INT NOT NULL AUTO_INCREMENT,
+    nome_forma_pagamento VARCHAR(100),
+    id_pedidos INT,
+    id_itens_do_pedido INT,
+    PRIMARY KEY(id_forma_pagamento),
+    FOREIGN KEY(id_pedidos) REFERENCES pedidos(id_pedidos),
+    FOREIGN KEY(id_itens_do_pedido) REFERENCES itens_do_pedidos(id_itens_do_pedido)
 );
 
 DELIMITER $$
@@ -124,10 +133,11 @@ BEGIN
 END $$
 
 DELIMITER ;
-drop procedure cadastrar_cliente;
+
 
 -- foram adionandos apenas 3 clientes no momento
-CALL cadastrar_cliente('Nathaly campos', 'naty', '1983-06-30', 'nathaly@gmail.com', '75956489', '76749334', 'Sim', '135', '467-000', 'FSA', 'BA');
+CALL cadastrar_cliente('Nathaly campos', 'naty', '1993-06-09', 'nathaly@gmail.com', '75956489', '76749334', 'Sim', '135', '467-000', 'FSA', 'BA');
+
 
 select*from clientes;
 select*from endereco;
