470a3352fa92d376dd73509c52cc68a60b34be36
Rodri.ys <141459048+rodrigosantossilv@users.noreply.github.com>
2023-11-24 14:02:59 -0300

    Update pastel-pj.sql

--- pastel-pj.sql
+++ pastel-pj.sql
@@ -2,144 +2,120 @@
 CREATE DATABASE Pastelaria;
 USE Pastelaria;
 
-CREATE TABLE categorias (
-    id_categoria INT NOT NULL AUTO_INCREMENT,
-    nome_categoria VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id_categoria)
+CREATE TABLE cliente(
+id_cliente int primary key auto_increment,
+nome_completo varchar(100),
+nome_preferido varchar(50),
+cpf VARCHAR(14) unique,
+data_nascimento date,
+telefone int(15),
+email varchar(100),
+bairro varchar(100),
+cidade varchar(50),
+estado varchar(2)
+);
+CREATE TABLE pasteis(
+id_pastel int primary key auto_increment,
+nome varchar(100),
+preco decimal(8,2),
+tamanho varchar(2),
+categoria varchar(20)
+);
+CREATE TABLE recheio(
+id_recheio int primary key auto_increment,
+nome varchar(50)
+);
+CREATE TABLE pedidos(
+id_pedido int primary key auto_increment,
+data_pedido date,
+id_cliente int not null,
+forma_pagamento varchar(20),
+foreign key(id_cliente) references cliente(id_cliente)
+);
+CREATE TABLE itens_pedido(
+id_item int primary key auto_increment,
+id_pedido int not null,
+id_produto int not null,
+quantidade int not null,
+foreign key(id_pedido)references pedidos(id_pedido),
+foreign key(id_produto)references produto(id_produto)
+);
+CREATE TABLE produto(
+id_produto int primary key auto_increment,
+nome varchar(50),
+preco decimal(8,2),
+volume varchar(20),
+tipo varchar(20)
 );
 
-CREATE TABLE recheios (
-    id_recheios INT NOT NULL AUTO_INCREMENT,
-    nome_recheio VARCHAR(100) NOT NULL,
-    id_categoria INT NOT NULL,
-    PRIMARY KEY (id_recheios),
-    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
-);
 
-CREATE TABLE pasteis (
-    id_pastel INT NOT NULL AUTO_INCREMENT,
-    nome_pastel VARCHAR(100) NOT NULL,
-    quant_estoque VARCHAR(100) NOT NULL, 
-    tamanho_pastel VARCHAR(20) NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
-    id_recheios INT NOT NULL,
-    id_categoria int not null,
-    PRIMARY KEY(id_pastel),
-    FOREIGN KEY(id_categoria) REFERENCES categorias(id_categoria
-    FOREIGN KEY(id_recheios) REFERENCES recheios(id_recheios)
-);
+DELIMITER //
 
-CREATE TABLE bebidas (
-    id_bebida INT NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    volume DECIMAL(10,2) NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
-    quant_estoque INT NOT NULL DEFAULT 0,
-    id_categoria INT NOT NULL,
-    PRIMARY KEY(id_bebida),
-    FOREIGN KEY(id_categoria) REFERENCES categorias(id_categoria)
-);
-
-CREATE TABLE clientes (
-    id_cliente INT NOT NULL AUTO_INCREMENT,
-    nome_completo VARCHAR(100) NOT NULL,
-    preferia_por_nome VARCHAR(100) NOT NULL,
-    cpf INT(11) UNIQUE,
-    data_nacimento DATE,
-    telefone INT(8) UNIQUE,
-    e_mail VARCHAR(100) UNIQUE,
-    PRIMARY KEY(id_cliente)
-);
-
-CREATE TABLE endereco (
-    id_endereco INT NOT NULL AUTO_INCREMENT,
-    id_cliente INT NOT NULL,
-    bairro VARCHAR(50),
-    numero VARCHAR(10),
-    cep VARCHAR(8),
-    municipio VARCHAR(50),
-    uf CHAR(2),
-    PRIMARY KEY(id_endereco),
-    FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
-);
-
-CREATE TABLE pedidos (
-    id_pedidos INT NOT NULL AUTO_INCREMENT,
-    id_cliente INT NOT NULL,
-    id_pastel INT NOT NULL,
-    id_bebidas INT NOT NULL,
-    quantidade INT NOT NULL,
-    data_pedido DATE,
-    PRIMARY KEY(id_pedidos),
-    FOREIGN KEY(id_bebidas) REFERENCES bebidas(id_bebida),
-    FOREIGN KEY(id_pastel) REFERENCES pasteis(id_pastel),
-    FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
-);
-
-CREATE TABLE tamanho (
-    id_tamanho INT NOT NULL AUTO_INCREMENT,
-    id_pastel INT NOT NULL,
-    PRIMARY KEY(id_tamanho),
-    FOREIGN KEY(id_pastel) REFERENCES pasteis(id_pastel)
-);
-
-CREATE TABLE itens_do_pedidos (
-    id_itens_do_pedido INT NOT NULL AUTO_INCREMENT,
-    id_pedidos INT NOT NULL,
-    quantidade_itens_pedidos INT NOT NULL,
-    PRIMARY KEY(id_itens_do_pedido),
-    FOREIGN KEY(id_pedidos) REFERENCES pedidos(id_pedidos)
-);
-
-CREATE TABLE formasPagamento (
-    id_forma_pagamento INT NOT NULL AUTO_INCREMENT,
-    nome_forma_pagamento VARCHAR(100),
-    id_pedidos INT,
-    id_itens_do_pedido INT,
-    PRIMARY KEY(id_forma_pagamento),
-    FOREIGN KEY(id_pedidos) REFERENCES pedidos(id_pedidos),
-    FOREIGN KEY(id_itens_do_pedido) REFERENCES itens_do_pedidos(id_itens_do_pedido)
-);
-
-DELIMITER $$
-
-CREATE PROCEDURE cadastrar_cliente(
+CREATE PROCEDURE AdicionarCliente(
     IN nome_completo VARCHAR(100),
-    IN preferia_por_nome VARCHAR(100),
-    IN data_nacimento DATE,
-    IN e_mail VARCHAR(100),
+    IN nome_preferido VARCHAR(50),
+    IN cpf VARCHAR(14),
+    IN data_nascimento DATE,
     IN telefone VARCHAR(15),
-    IN cpf CHAR(14),
+    IN email VARCHAR(100),
     IN bairro VARCHAR(50),
-    IN numero VARCHAR(10),
-    IN cep VARCHAR(8),
-    IN municipio VARCHAR(50),
-    IN uf CHAR(2)
+    IN cidade VARCHAR(50),
+    IN estado VARCHAR(2)
 )
 BEGIN
-    DECLARE id_cliente INT;
+    INSERT INTO cliente (nome_completo,nome_preferido,cpf,data_nascimento,telefone,email,bairro,cidade,estado) 
+    VALUES (nome_completo,nome_preferido,cpf,data_nascimento,telefone,email,bairro,cidade,estado);
+END  //
+DELIMITER ;
+CALL AdicionarCliente('Rodigo da Silva', 'Rodrigo', '123.456.789-01', '2005-05-09', '123456789', 'rodrigo@email.com', 'humildes', 'FSA', 'BA');
+CALL AdicionarCliente('Carlos henrique', 'Carlos', '345.609.456-01', '2003-08-03', '750304859', 'carlos@email.com', 'Aviario', 'FSA', 'BA');
+CALL AdicionarCliente('Yasmin nere', 'Yasmin', '503.987.545-01', '2004-03-14', '75395030', 'carlos@email.com', 'feira 7', 'FSA', 'BA');
+select*from cliente;
 
-    START TRANSACTION;
-
-
-    INSERT INTO Clientes(nome_completo, preferia_por_nome, data_nacimento, e_mail, telefone, cpf)
-    VALUES (nome_completo, preferia_por_nome, data_nacimento, e_mail, telefone, cpf);
-
-
-    SET id_cliente = LAST_INSERT_ID();
-
-    INSERT INTO Endereco(id_cliente, bairro, numero, cep, municipio, uf)
-    VALUES (id_cliente, bairro, numero, cep, municipio, uf);
-
-    COMMIT;
-END $$
-
+DELIMITER //
+CREATE PROCEDURE AdicionarPastel(
+    IN nome VARCHAR(100),
+    IN preco DECIMAL(8,2),
+    IN tamanho VARCHAR(2),
+    IN categoria VARCHAR(20)
+)
+BEGIN
+    INSERT INTO pasteis (nome, preco, tamanho, categoria) 
+    VALUES (nome, preco, tamanho, categoria);
+END  //
 DELIMITER ;
 
+CALL AdicionarPastel('Pastel de carne', 5.00, 'M', 'normal');
+CALL AdicionarPastel('Pastel de frango', 6.00, 'G', 'normal');
+CALL AdicionarPastel('Pastel de queijo', 6.00, 'G', 'normal');
+CALL AdicionarPastel('Pastel de palmito', 3.00, 'P', 'vegano');
+CALL AdicionarPastel('Pastel do popeye', 7.50, 'G', 'vegano');
+CALL AdicionarPastel('Pastel de espinafre', 5.50, 'G', 'vegano');
+CALL AdicionarPastel('Pastel de carne seca', 6.50, 'M', 'zero lactose');
+CALL AdicionarPastel('Pastel de camarão', 6.50, 'M', 'zero lactose');
+CALL AdicionarPastel('Pastel de bacalhau', 6.50, 'M', 'zero lactose');
 
--- foram adionandos apenas 3 clientes no momento
-CALL cadastrar_cliente('Nathaly campos', 'naty', '1993-06-09', 'nathaly@gmail.com', '75956489', '76749334', 'Sim', '135', '467-000', 'FSA', 'BA');
+select*from pasteis;
 
+DELIMITER //
+CREATE PROCEDURE AdicionarProduto(   
+    IN p_nome VARCHAR(50),
+    IN p_preco DECIMAL(8,2),
+    IN p_volume VARCHAR(20),
+    IN p_tipo VARCHAR(20)
+)
+BEGIN
+    INSERT INTO produto (nome, preco, volume, tipo) 
+    VALUES (p_nome, p_preco, p_volume, p_tipo);
+END //
+DELIMITER ;
 
-select*from clientes;
-select*from endereco;
+CALL AdicionarProduto('Suco de laranja', 2.00, '1L', 'bebida');
+CALL AdicionarProduto('Suco de laranja', 5.00, '2L', 'bebida');
+CALL AdicionarProduto('Refrigerante de cola', 3.50, '1L', 'bebida');
+CALL AdicionarProduto('Refrigerante de guaraná', 4.00, '1L', 'bebida');
+CALL AdicionarProduto('Suco de morango', 7.00, '2L', 'bebida');
+CALL AdicionarProduto('Suco de limão', 3.50, '1L', 'bebida');
+CALL AdicionarProduto('Tubaina', 1.50, '350 ML', 'bebida');
+
+select*from produto;
